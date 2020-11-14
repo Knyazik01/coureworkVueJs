bef4301bfa29a4b017771aa9617483798b08fef5
Knyazik01 <ndrwkn2001@gmail.com>
2020-11-14 16:45:33 +0200

    Footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,8 +1,12 @@
 <template>
-  <footer>
-    <FooterLink link-content="Knyazik01" link-url="#"/>
-    <FooterLink :link-content="'github.png'" link-url="#" link-type="img"/>
-    <FooterLink :link-content="'\u00A9 Viktor Kniahnitskyi'"/>
+  <footer class="footer">
+    <div class="footer_left">
+      <FooterLink :link-content="'github.png'" link-url="https://github.com/Knyazik01" link-type="img" link-label="Knyazik01" new-tab/>
+    </div>
+
+    <div class="footer_right">
+      <FooterLink :link-content="'\u00A9 Viktor Kniahnitskyi'"/>
+    </div>
   </footer>
 </template>
 
@@ -17,6 +21,17 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.footer {
+  margin: 10px 15px 0 15px;
+  padding: 10px;
+  border-top: 2px dashed white;
+  display: flex;
+  flex-direction: row;
+  font-size: 1.2em;
+  justify-content: space-between;
+  align-items: center;
+}
 
 </style>
--- src/components/footer/FooterLink.vue
+++ src/components/footer/FooterLink.vue
@@ -1,9 +1,10 @@
 <template>
-  <a v-if="linkUrl" :href="`${linkUrl}`">
-    <img :src="getImgUrl(linkContent)" v-if="linkType === 'img'">
-    <span v-else>{{linkContent}}</span>
-    </a>
-  <span v-else class="link">{{linkContent}}</span>
+  <a v-if="linkUrl" :href="`${linkUrl}`" class="footer_text footer_link" :target="(newTab) ? '_blank' : false">
+    <img v-if="linkType === 'img'" :src="getImgUrl(linkContent)" :alt="linkContent" class="footer_logo">
+    <span v-else>{{ linkContent }}</span>
+    <span v-if="linkType === 'img' && linkLabel">{{ linkLabel }}</span>
+  </a>
+  <span v-else class="footer_text">{{ linkContent }}</span>
 </template>
 
 <script>
@@ -13,20 +14,38 @@ export default {
     linkUrl: String,
     linkContent: String,
     linkType: String,
+    linkLabel: String,
+    newTab: Boolean,
   },
   methods: {
     getImgUrl(pic) {
-      return require('@/assets/'+pic)
+      return require('@/assets/' + pic)
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.link {
-  &:hover {
-    color: #42b983;
+
+.footer {
+  &_text {
+    margin-top: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #a9a9a9;
+    text-decoration: none;
   }
-  color: red;
+
+  &_link:hover {
+      color: #fff;
+      text-decoration: none;
+  }
+
+  &_logo {
+    width: 1em;
+    margin-right: 8px;
+  }
 }
+
 </style>
